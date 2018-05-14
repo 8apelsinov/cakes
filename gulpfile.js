41d51244f77710143b7b9060c4a33271b7a63b6e
8apelsinov <8apelsinov@gmail.com>
2018-05-14 19:34:35 +0300

    14may

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+gulp.task("less", function() {
+    gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+        autoprefixer()
+    ]))
+    .pipe(gulp.dest("css"));
+ });
